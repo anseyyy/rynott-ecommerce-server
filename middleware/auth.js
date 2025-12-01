5a0f053ec56503ff36045c35ea96359e9068a0ae
anseyyy <ahamedansil43@gmail.com>
2025-12-01 20:57:46 +0530

    Fix JWT_SECRET fallback in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ const User = require("../models/User");
 
 const protect = async (req, res, next) => {
   let token;
+  const jwtSecret = process.env.JWT_SECRET || "rynott_super_secret_jwt_key_change_in_production_2024";
 
   if (
     req.headers.authorization &&
@@ -13,7 +14,7 @@ const protect = async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
 
       // Verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(token, jwtSecret);
 
       // Get user from the token
       req.user = await User.findById(decoded.id).select("-password");
